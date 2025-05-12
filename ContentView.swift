8cd951c612e639d94cf1bc5241a606a99d7d47e3
김찬우 <chanwoo219@gmail.com>
2025-05-12 19:32:31 +0900

    Add files via upload

--- ContentView.swift
+++ ContentView.swift
@@ -14,19 +14,15 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(toDos) { todo in
+                ForEach($toDos) {$todo in
                     HStack {
-                        Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                        Button(action:{
+                            todo.isCompleted.toggle()
+                        }){
+                            Image(systemName: todo.isCompleted ? "checkmark.circle" : "circle")}
                         Spacer()
                         Text(todo.title)
                     }
-                    // 할 일 완료 체크표시
-                    .onTapGesture {
-                        if let i = toDos.firstIndex(where: { $0.id == todo.id }) {
-                            toDos[i].isCompleted.toggle()
-                        }
-                    }
-                    // 할 일 삭제
                     .swipeActions {
                         Button(role: .destructive) {
                             toDos.removeAll { $0.id == todo.id }
